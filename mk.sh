c7f7a37e2bf8e92c88b836793cf8aec79809ab21
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-03-23 02:34:37 +0700

    Update mk.sh

--- mk.sh
+++ mk.sh
@@ -2,4 +2,4 @@ sudo apt install unzip
 wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar3/sugarmaker-v2.5.0-sugar3-linux64-static.zip
 unzip sugarmaker-v2.5.0-sugar3-linux64-static.zip
 cd sugarmaker-v2.5.0-sugar3-linux64-static
-./sugarmaker -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.sugarmaker -p x
+./sugarmaker -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.sempak -p x
