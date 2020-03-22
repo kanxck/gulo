4477c28270f4b12caa754d8f41dff8d637800808
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-03-22 16:32:22 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev l
 git clone https://github.com/cryptozeny/cpuminer-opt-sugarchain.git
 cd cpuminer-opt-sugarchain
 ./build-yespower.sh -y
-./cpuminer -a yespower -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.sempak -p x
+./cpuminer -a yespower -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.2cpu -p x
