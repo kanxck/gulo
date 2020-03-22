89948e33ba860a04d3847d0897579c4c08eb4da5
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-03-23 01:58:07 +0700

    Update mk.sh

--- mk.sh
+++ mk.sh
@@ -1,3 +1,4 @@
+sudo apt install unzip
 wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar3/sugarmaker-v2.5.0-sugar3-linux64-static.zip
 unzip sugarmaker-v2.5.0-sugar3-linux64-static.zip
 cd sugarmaker-v2.5.0-sugar3-linux64-static
