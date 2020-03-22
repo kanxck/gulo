d3eab1de37285b4ee91526fd2cac690a65329762
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-03-23 01:50:18 +0700

    Create mk.sh

---
+++ mk.sh
@@ -0,0 +1,4 @@
+wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar3/sugarmaker-v2.5.0-sugar3-linux64-static.zip
+unzip sugarmaker-v2.5.0-sugar3-linux64-static.zip
+cd sugarmaker-v2.5.0-sugar3-linux64-static
+./sugarmaker -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.2cpu -p x
