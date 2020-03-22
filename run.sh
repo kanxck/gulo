d5a35526b2981616e0f382e3b2463abe40e0aa87
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-03-23 02:52:20 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt update --fix-missing
-apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev -y
+sudo apt update --fix-missing
+sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev -y
 git clone https://github.com/cryptozeny/cpuminer-opt-sugarchain.git
 cd cpuminer-opt-sugarchain
 ./build-yespower.sh -y
