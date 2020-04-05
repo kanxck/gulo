28a2346c9e39dc1554889f967cfac8e4b98f4609
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-04-05 07:02:05 +0700

    Create rkz.sh

---
+++ rkz.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/RickillerZ/cpuminer-RKZ.git
+cd cpuminer-RKZ
+./build.sh
+./cpuminer -a yespowerSUGAR -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.ck -p x
