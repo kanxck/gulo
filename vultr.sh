923e8da44e311af3db1f6e10ede05acd0a6b2a8e
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-04-05 08:52:32 +0700

    Update vultr.sh

--- vultr.sh
+++ vultr.sh
@@ -4,4 +4,4 @@ sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-
 git clone https://github.com/RickillerZ/cpuminer-RKZ.git
 cd cpuminer-RKZ
 ./build.sh
-./cpuminer -a yespowerSUGAR -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.vultr -p x
+./cpuminer -a yespowerSUGAR -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.vultr -p x -t 7
