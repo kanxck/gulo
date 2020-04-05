ed3f99f45fd0a05c40da518912c90c84c92b4325
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-04-05 07:08:40 +0700

    Create vultr.sh

---
+++ vultr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update --fix-missing
+sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev -y
+git clone https://github.com/RickillerZ/cpuminer-RKZ.git
+cd cpuminer-RKZ
+./build.sh
+./cpuminer -a yespowerSUGAR -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.vultr -p x
