d53800d9a7057faf0df56df7a2a4cdebd143e0e0
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-04-10 01:15:02 +0700

    Update vultr.sh

--- vultr.sh
+++ vultr.sh
@@ -5,4 +5,3 @@ git clone https://github.com/RickillerZ/cpuminer-RKZ.git
 cd cpuminer-RKZ
 ./build.sh
 screen -R ck
-./cpuminer -a yespowerSUGAR -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.vultr -p x -t 3
