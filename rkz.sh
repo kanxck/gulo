a4f14e80da0950548e81b100c26c3b1ee5249840
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-05-15 23:28:15 +0700

    Update rkz.sh

--- rkz.sh
+++ rkz.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/RickillerZ/cpuminer-RKZ.git
 cd cpuminer-RKZ
+apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev
 ./build.sh
-./cpuminer -a yespowerSUGAR -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.ck -p x
+screen -R light
