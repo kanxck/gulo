099d68288677353fdc10a91387fac1126d6f2525
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-05-20 09:00:32 +0700

    Update opt.sh

--- opt.sh
+++ opt.sh
@@ -1,3 +1,4 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
-tar -xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qqa4mvu0gj6razf9tnkuum8n7u0h76r8pn43wpa.ck -p x
+apt update && apt-get -y install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
+git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
+./build.sh
