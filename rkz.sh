9a66d2931b5daa857fc3c3ab1851a054813d5119
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-05-22 15:14:10 +0700

    Update rkz.sh

--- rkz.sh
+++ rkz.sh
@@ -1,5 +1,4 @@
-apt update && apt -y upgrade
+apt update && apt -y upgrade && apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev
 git clone https://github.com/RickillerZ/cpuminer-RKZ.git
 cd cpuminer-RKZ
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev
 ./build.sh
