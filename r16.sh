539bf04f90e91529e242cfbcf166116a78ca0263
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-05-28 03:28:15 +0700

    Update r16.sh

--- r16.sh
+++ r16.sh
@@ -1,3 +1,3 @@
 apt-get -y install build-essential libcurl4-openssl-dev
-git clone https://github.com/WIDE888/cpuminer-yescryptr16 && cd cpuminer-yescryptr16 && ./autogen.sh && /configure CFLAGS="-O3"
+git clone https://github.com/WIDE888/cpuminer-yescryptr16 && cd cpuminer-yescryptr16 && ./autogen.sh && ./configure CFLAGS="-O3"
 make
