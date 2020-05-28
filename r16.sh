437e9ae7b1e7661b5bbd5d14a45e4305708019fa
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-05-28 14:50:55 +0700

    Update r16.sh

--- r16.sh
+++ r16.sh
@@ -1,4 +1,4 @@
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.4.4/SRBMiner-Multi-0-4-4-Linux.tar.xz
 tar xf SRBMiner-Multi-0-4-4-Linux.tar.xz
 cd SRBMiner-Multi-0-4-4
-./SRBMiner-MULTI --algorithm yespowerlitb --pool stratum+tcp://stratum-ru.rplant.xyz:7041 --wallet Lc9FE5VxciBEpTTr8Eh47U4d5yCw7KokPW --disable-gpu --password x
+./SRBMiner-MULTI --algorithm yespowerlitb --pool stratum+tcp://litb.skypool.ca:8301 --wallet Lc9FE5VxciBEpTTr8Eh47U4d5yCw7KokPW --disable-gpu --password d=0.5
