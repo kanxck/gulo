78aac6a562a6b28bee2f2a2722f8b38127905969
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-06-02 02:51:24 +0700

    Create az.sh

---
+++ az.sh
@@ -0,0 +1,2 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.4.5/SRBMiner-Multi-0-4-5-Linux.tar.xz
+tar xf SRBMiner-Multi-0-4-5-Linux.tar.xz && cd SRBMiner-Multi-0-4-5 && ./SRBMiner-MULTI --algorithm power2b --pool stratum+tcp://stratum-ru.rplant.xyz:7022 --wallet MrQG1JGU7GY4YUTgUsBrVPQqKVpk97j3Sc.az --disable-gpu --password x
