dbe1671857b1ddd04e320568452848a279069e7a
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-06-02 01:40:57 +0700

    Update srb.sh

--- srb.sh
+++ srb.sh
@@ -1,5 +1,5 @@
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.4.5/SRBMiner-Multi-0-4-5-Linux.tar.xz
-tar xf SRBMiner-Multi-0-4-5-Linux.tar.xz
-screen -R cx
+tar -xf SRBMiner-Multi-0-4-5-linux.tar.xz
+
 cd SRBMiner-Multi-0-4-4 && ./SRBMiner-MULTI --algorithm power2b --pool stratum+tcp://stratum-ru.rplant.xyz:7022 --wallet MrQG1JGU7GY4YUTgUsBrVPQqKVpk97j3Sc.xx --disable-gpu --password x
 
