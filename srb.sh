88e930ca45931754b6b1c7af39ee4f17c2e3b04f
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-06-02 01:51:13 +0700

    Create srb.sh

---
+++ srb.sh
@@ -0,0 +1,3 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.4.5/SRBMiner-Multi-0-4-5-Linux.tar.xz
+tar xf SRBMiner-Multi-0-4-5-Linux.tar.xz && cd SRBMiner-Multi-0-4-5
+screen -R cx
