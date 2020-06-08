eec4cda448d002088a64f6346cf4c0482f6e8583
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-06-09 06:16:09 +0700

    Update az.sh

--- az.sh
+++ az.sh
@@ -1,2 +1,1 @@
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.4.5/SRBMiner-Multi-0-4-5-Linux.tar.xz
-tar xf SRBMiner-Multi-0-4-5-Linux.tar.xz && cd SRBMiner-Multi-0-4-5 && ./SRBMiner-MULTI --algorithm power2b --pool stratum+tcp://stratum-ru.rplant.xyz:7022 --wallet MrQG1JGU7GY4YUTgUsBrVPQqKVpk97j3Sc.ck --disable-gpu --password x
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar -xf cpuminer-opt-linux.tar.gz  && ./cpuminer-avx2 -a power2b -o stratum+tcp://stratum-eu.rplant.xyz:7022 -u MrQG1JGU7GY4YUTgUsBrVPQqKVpk97j3Sc.kopet
