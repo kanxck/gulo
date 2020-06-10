70b7720124a35cd83cf632f9c51ff4b0c201a89f
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-06-10 16:34:20 +0700

    Create opt16.sh

---
+++ opt16.sh
@@ -0,0 +1,5 @@
+apt-get install build-essential libcurl4-openssl-dev autotools-dev automake
+git clone https://github.com/crypto-jeronimo/cpuminer-yescrypt-r16.git
+cd cpuminer-yescrypt-r16
+./autogen.sh
+./configure CFLAGS="-O3"
