eeb6d4a5a7e93fb3a55d8a56a6ef7711d031d1dc
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-06-10 15:39:08 +0700

    Create gl.sh

---
+++ gl.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-avx2 -a yescryptr16 -o stratum+tcp://pooler.rtid-platform.web.id:3030 -u GVH1ck3UGYgQeM6631Lqc8azDErQ3ERw7k.ck
