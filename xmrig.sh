61044d51b3438c7eab7209c7448bfb31551d906c
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-06-27 01:30:54 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,5 +1,5 @@
-mkdir ck && sudo upgrade -y && sudo apt-get -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-cd ck && git clone https://github.com/xmrig/xmrig.git
+mkdir tre && sudo upgrade -y && sudo apt-get -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+cd tre && git clone https://github.com/xmrig/xmrig.git
 cd xmrig/src && nano donate.h
 cd .. && mkdir ck && cd ck
 cmake ..
