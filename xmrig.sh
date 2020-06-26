ac4d150cd77b1f155b7d9008708db878e4895844
kanx cunxkrinx <46498863+kanxck@users.noreply.github.com>
2020-06-27 01:13:02 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt upgrade -y && sudo apt-get -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 mkdir ck 
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir ck && cd ck
